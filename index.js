1978866667737fb3dedc912870fbf027c9bdca4b
EblissYisse <segoviavenales@hotmail.com>
2023-03-31 09:33:45 -0500

    v1

---
+++ index.js
@@ -0,0 +1,77 @@
+//Construiremos la API y la logica de negocios en este index
+//Paso 1: Crear un objeto que represente a la biblioteca express
+
+const express=require('express'); //objeto que representa la biblioteca express
+
+//paso 2: Crear un objeto que represente nuesra aplicacion 
+
+const app= express(); //representa una aplicacion creada apartir de la biblioteca express
+
+app.use(express.json());//le decimos que la app pueda intepretar las peticiones en formato json
+
+//Definir los entry point (puntos de entrada a la API), 
+//En node estos puntos de entrada se denomina definir la ruta, la URL en donde va a responder la API
+//Metodo: forma en que un cliente le envia datos a un servidor.
+//http:localhost:3001/sumar
+
+app.get(
+    '/sumar',
+    //se requieren dos objetos, 1 representando la peticion (req) y otro un objeto representando la respuesta(rest)
+    (req, res)=>{
+        //to do: aqui va el procesamiento de la peticion a esta ruta
+        console.log("Alguien esta conetandose a esta ruta");
+        res.json(req.body);
+    }
+);
+//Crear otra ruta
+//para reconocer el metodo post debemos utlizar una herramienta en java o utlizar insomnia o postaman
+app.post(
+    '/restar',
+    (req, res)=>{
+        // 1 forma de hacerlo mas facil: deconstruccion de un objeto en json
+        const {numero_1, numero_2}= req.body;
+        const resultado=numero_1 - numero_2;
+        res.json(resultado);
+
+        //creamos el algoritmo para que reste los numeros
+        /*const n1= req.body.numero_1;
+        const n2= req.body.numero_2;
+        const resultado=n1 - n2;
+        res.json(resultado);
+        */
+    }
+);
+
+app.post(
+    '/dividir',
+    (req,res)=>{
+        let resultado;
+
+        try{
+            const {numero_1, numero_2}= req.body;
+            resultado=numero_1/numero_2;
+        }catch(error){
+            //gestionar el error
+            resultado=error;
+        }
+
+        res.json(resultado);
+
+    }
+)
+//paso 3: Crear un servidor o un servicio para escuchar peticiones mediante express
+
+//metodo listen tiene dos argumentos o parametros, el 1 es el puerto donde quiero escuchar esas peticiones, el 2 es el hostname: nombre de dominio
+app.listen(
+    3001,
+    //funcion de colback: tiene un formato tipo flecha, tambien puede ser anonima
+    ()=>{
+      console.log("Servidor ejecutandose en el puerto 3001");
+
+    }
+
+)
+
+/*Taller: Como crear una ruta que yo 
+le pueda pasar un numero indeterminado de datos
+*/
