5afdd44befc716de95f4e49740002fbbcd739eb2
EblissYisse <segoviavenales@hotmail.com>
2023-04-02 22:10:50 -0500

    v2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //Construiremos la API y la logica de negocios en este index
 //Paso 1: Crear un objeto que represente a la biblioteca express
 
@@ -9,18 +10,30 @@ const app= express(); //representa una aplicacion creada apartir de la bibliotec
 
 app.use(express.json());//le decimos que la app pueda intepretar las peticiones en formato json
 
+//DEBEMOS CONFIGURAR EL CORS, se puede hacer ruta por ruta o de entrada para todas
+//"*" dejar el acceso publico 
+app.use( function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Headers", "Content-type");
+    next();
+}
+);
+
 //Definir los entry point (puntos de entrada a la API), 
 //En node estos puntos de entrada se denomina definir la ruta, la URL en donde va a responder la API
 //Metodo: forma en que un cliente le envia datos a un servidor.
-//http:localhost:3001/sumar
+//http://localhost:3002/api/sumar
 
-app.get(
-    '/sumar',
+app.post(
+    '/api/sumar',
     //se requieren dos objetos, 1 representando la peticion (req) y otro un objeto representando la respuesta(rest)
     (req, res)=>{
         //to do: aqui va el procesamiento de la peticion a esta ruta
         console.log("Alguien esta conetandose a esta ruta");
-        res.json(req.body);
+        const {numero_1, numero_2}= req.body;
+        const resultado=parseFloat(numero_1) + parseFloat(numero_2);
+        res.json(resultado);
     }
 );
 //Crear otra ruta
@@ -63,10 +76,10 @@ app.post(
 
 //metodo listen tiene dos argumentos o parametros, el 1 es el puerto donde quiero escuchar esas peticiones, el 2 es el hostname: nombre de dominio
 app.listen(
-    3001,
+    3002,
     //funcion de colback: tiene un formato tipo flecha, tambien puede ser anonima
     ()=>{
-      console.log("Servidor ejecutandose en el puerto 3001");
+      console.log("Servidor ejecutandose en el puerto 3002");
 
     }
 
